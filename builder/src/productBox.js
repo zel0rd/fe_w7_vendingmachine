dcb71e22133a3b27554f4d3f4ac8c221d08ebb09
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-03-23 18:22:09 +0900

    [ADD] html structure and css

---
+++ builder/src/productBox.js
@@ -0,0 +1,14 @@
+import {$,$$} from "./util.js";
+
+const productBox = $(".product__box");
+const renderProduct=()=> {
+    const productArr = ["에끌레어","몽블랑","밀푀유","수플레","크림브륄레","크레페","타르트","파르페","그라니타","푸딩","스콘","휘낭시에"]
+    let inp="";
+    productArr.map((v, i) => {
+        inp+=`<div id=${v} class="product w-1/3 py-8 border-6 border-yellow-200 bg-yellow-100 hover:bg-yellow-200">${v}</div>`;
+    })
+    productBox.innerHTML=inp;
+
+}
+
+export { renderProduct };
---
+++ builder/src/util.js
@@ -0,0 +1,6 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+
+export {$,$$};
