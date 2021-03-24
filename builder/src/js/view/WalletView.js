96890bbfe778c665b3bac92bcd4264c9c605414f
younggil <zelord.kwoun@gmail.com>
2021-03-24 23:23:21 +0900

    [ADD] When Model Data Changed, Noti Changed View Render.

--- builder/src/js/view/WalletView.js
+++ builder/src/js/view/WalletView.js
@@ -2,25 +2,34 @@ import {$,$$} from "../util/util";
 
 const moneyCountBox = $(".money__count__box");
 
-
 class WalletView {
     //WalletModel의 init data가져오기
     constructor() {
     }
     //update
-    update() {
-        const tencount = $("#10");
-
-
+    update(data) {
+        console.log(data)
+        console.log("updated")
+        this.render(data);
     }
     //초기 money 개수 렌더링
-    async render() {
-        let moneyCountInitArr = [1, 2, 5, 10, 20];
-        let inp = moneyCountInitArr.reduce((acc, cur) => {
-            acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
-            return acc;
-        },"")
-        moneyCountBox.innerHTML = inp;
+    async render(data) {
+        if(data){
+            let coins = Object.values(data)
+            let moneyCountInitArr = coins;
+            let inp = moneyCountInitArr.reduce((acc, cur) => {
+                acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
+                return acc;
+            },"")
+            moneyCountBox.innerHTML = inp;
+        } else {
+            let moneyCountInitArr = [1, 2, 5, 10, 20];
+            let inp = moneyCountInitArr.reduce((acc, cur) => {
+                acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
+                return acc;
+            },"")
+            moneyCountBox.innerHTML = inp;
+        }
     }
 
     //moeny 개수 data 변경 시 렌더링 이벤트
@@ -31,17 +40,14 @@ class WalletView {
 
         for (var i of moneyCountArr) {
             i.addEventListener("click", function () {
-                console.log(i);
+                console.log(i)
+                this.render;
             })
         }
         
 
     }
     
-
-    
-
-
 }
 
 // const renderMoneyCount = () => {
