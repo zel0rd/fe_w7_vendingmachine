f55c0cc2f0411bc70021d5ac3f84c00155359add
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-03-25 01:18:50 +0900

    [REFACTOR] WalletView.js

--- builder/src/js/view/WalletView.js
+++ builder/src/js/view/WalletView.js
@@ -1,57 +1,37 @@
 import {$,$$} from "../util/util";
-
 const moneyCountBox = $(".money__count__box");
 
 class WalletView {
     //WalletModel의 init data가져오기
-    constructor() {
+    constructor (WalletModel) {
+        this.WalletModel = WalletModel;
     }
     //update
-    update(data) {
-        console.log(data)
-        console.log("updated")
-        this.render(data);
+    update() {
+        this.setEvent();
     }
     //초기 money 개수 렌더링
-    async render(data) {
-        if(data){
-            let coins = Object.values(data)
-            let moneyCountInitArr = coins;
-            let inp = moneyCountInitArr.reduce((acc, cur) => {
-                acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
-                return acc;
-            },"")
-            moneyCountBox.innerHTML = inp;
-        } else {
-            let moneyCountInitArr = [1, 2, 5, 10, 20];
-            let inp = moneyCountInitArr.reduce((acc, cur) => {
-                acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
-                return acc;
-            },"")
-            moneyCountBox.innerHTML = inp;
-        }
+    async render() {
+        let coins = Object.values(this.WalletModel.wallet.myMoney);
+        let inp = coins.reduce((acc, cur) => {
+            acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
+            return acc;
+        },"")
+        moneyCountBox.innerHTML = inp;
     }
 
-    //moeny 개수 data 변경 시 렌더링 이벤트
+    //money 개수 data 변경 시 렌더링 이벤트
     async setEvent() {
-        await this.render();
-        // const moneyCountArr = $$(".money__cnt");
+        this.render();
         const moneyCountArr = document.querySelectorAll(".money__cnt");
+        for (let i = 0; i < moneyCountArr.length; i++){
+            let Walletdata = this.WalletModel;
+            moneyCountArr[i].addEventListener("click", function () {
+                Walletdata.decrement(Object.keys(Walletdata.wallet.myMoney)[i]);
 
-        for (var i of moneyCountArr) {
-            i.addEventListener("click", function () {
-                console.log(i)
-                this.render;
             })
         }
-        
-
-    }
-    
+    } 
 }
-
-// const renderMoneyCount = () => {
-// }
-
 
 export { WalletView };
