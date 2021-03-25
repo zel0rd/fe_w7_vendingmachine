19ae567b0b4f96c4d6cdee3b1c3c1d22f5d27281
younggil <zelord.kwoun@gmail.com>
2021-03-26 00:23:46 +0900

    [ADD] unused async delete

--- builder/src/js/view/WalletView.js
+++ builder/src/js/view/WalletView.js
@@ -27,7 +27,7 @@ class WalletView {
     }
 
     //초기 money 개수 렌더링
-    async render() {
+    render() {
         let coins = Object.values(this.WalletModel.wallet.myMoney);
         let inp = coins.reduce((acc, cur) => {
             acc += `<div id=${cur} class="money__cnt h-1/6 py-4 my-3 min-w-150 border-4 border-dashed rounded-2xl border-yellow-200 border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${cur}</div>`;
@@ -41,7 +41,7 @@ class WalletView {
     }
 
     //money 개수 data 변경 시 렌더링 이벤트
-    async setEvent() {
+    setEvent() {
         const moneyCountArr = document.querySelectorAll(".money__cnt");
         for (let i = 0; i < moneyCountArr.length; i++){
             let Walletdata = this.WalletModel;
