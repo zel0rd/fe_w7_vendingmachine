c86be096b72add8ad30b09ee6ff36b48cb729879
younggil <zelord.kwoun@gmail.com>
2021-03-25 16:18:58 +0900

    [ADD] Product buy function added

--- builder/src/js/view/ProductView.js
+++ builder/src/js/view/ProductView.js
@@ -10,23 +10,66 @@ class ProductView {
     }
 
     update(){
-        // console.log(this)
-        this.render()
+        this.setEvent();
     }
 
-    getInsertMoney(){
-        console.log(this.WalletModel)
+    getInsertMoney() {
+        let totalMoney=0;
+        for (const [key, value] of Object.entries(this.WalletModel.insert.myInsert))
+        {
+            totalMoney += key * value;
+        }
+        return totalMoney;
     }
 
     render() {
         const vendingBox = $(".vending__box")
+        const insertMoney = this.getInsertMoney();
         let menu = this.ProductModel.menu;
         let inp = "";
         for (const [key, value] of Object.entries(menu)) {
-            inp+=`<div id=${key} class="product w-1/4 h-1/4 m-2 py-8 min-w-100 border-4 border-dashed rounded-2xl border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${key}<br>${value}</div>`;
+            if(value <= insertMoney){
+                // 활성화 버튼 div class 추가
+                inp+=`<div id=${key} class="product w-1/4 h-1/4 m-2 py-8 min-w-100 border-4 border-dashed rounded-2xl border-yellow-200 bg-green-100 hover:bg-yellow-200 shadow-md">${key}<br>${value}</div>`;
+            } else {
+                // 비활성화 버튼 div class 추가
+                inp+=`<div id=${key} class="product w-1/4 h-1/4 m-2 py-8 min-w-100 border-4 border-dashed rounded-2xl border-yellow-200 bg-yellow-100 hover:bg-yellow-200 shadow-md">${key}<br>${value}</div>`;
+            }
+            
         }
         vendingBox.innerHTML=inp;
     }
+
+    buy(walletModel, price){
+        return function(walletModel, price){
+            console.log(walletModel)
+            walletModel.buy(price)
+        }
+    }
+
+    fail(){
+        return function() {
+            console.log("실패!!!")
+        }
+    }
+
+
+    setEvent() {
+        this.render();
+        const products = document.querySelectorAll(".product")
+        const insertMoney = this.getInsertMoney();
+        const buyCallBack = this.buy()
+        const failCallBack = this.fail()
+        const walletModel = this.WalletModel;
+        for (let i = 0; i < products.length; i++){
+            let productModel = this.ProductModel
+            products[i].addEventListener("click", function () {
+                let price = Object.values(productModel.menu)[i]
+                return (price <= insertMoney) ? buyCallBack(walletModel, price) : failCallBack()
+            })
+        }
+    }
+
 }
 
 export { ProductView }
